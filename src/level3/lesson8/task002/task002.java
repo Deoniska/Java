853c75a8bc663b5d5834295a8fae037fb37acad9
denis <deoniska@me.com>
2018-11-23 15:04:16 +0300

    holiday

---
+++ src/level3/lesson8/task002/task002.java
@@ -0,0 +1,12 @@
+package level3.lesson8.task002;
+/*
+Предсказание на будущее
+*/
+
+import java.io.*;
+
+public class task002 {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
