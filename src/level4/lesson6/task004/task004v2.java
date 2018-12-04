713ec40d9f9a50450e394a926a442cb91147ca14
denis <deoniska@me.com>
2018-12-04 07:17:52 +0300

    morning

--- src/level4/lesson6/task004/task004v2.java
+++ src/level4/lesson6/task004/task004v2.java
@@ -8,24 +8,28 @@ import java.io.*;
 
 public class task004v2 {
 
-    public static void main(String[] args) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
         //напишите тут ваш код
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String name1 = reader.readLine();
-        String name2 = reader.readLine();
+        String name1 = read();
+        String name2 = read();
 
-        int a=name1.length();
-        int b=name2.length();
+        int a = name1.length();
+        int b = name2.length();
 
-        if (name1.equals(name2))
-        {
+
+        if (name1.equals(name2)) {
             System.out.println("Имена идентичны");
-        }
-        else
-        if (a == b)
-        {
+        } else if (a == b) {
             System.out.println("Длины имен равны");
         }
     }
+
+    public static String read() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        return s;
+    }
 }
+
+
+
