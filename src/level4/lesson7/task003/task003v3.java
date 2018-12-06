d8258d27523ee99ca2fbfabb4c0dfa9b378660c5
denis <deoniska@me.com>
2018-12-06 07:33:00 +0300

    Thursday morning

--- src/level4/lesson7/task003/task003v3.java
+++ src/level4/lesson7/task003/task003v3.java
@@ -1,14 +1,13 @@
 package level4.lesson7.task003;
-
 /*
 Положительное число
 */
 
 
-        import java.io.BufferedReader;
-        import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-public class task003 {
+public class task003v3 {
     public static void main (String[] args) throws Exception {
 
         int a = read();
@@ -22,14 +21,10 @@ public class task003 {
     public static int count (int a, int b, int c) {
         int summ = 0;
 
-        if ((a >0)&&(b>0)&&(c>0))  summ=3;
-        else if ((a>0)&&(b>0)) summ =2;
-        else if ((a>0)&&(c>0)) summ=2;
-        else if ((b>0)&&(c>0)) summ=2;
-        else if ((a>0)|(b>0)|(c>0)) summ=1;
-        else if (a == 0& b==0 & c==0) summ=0;
 
-
+        if (a>0) summ++;
+        if (b>0) summ++;
+        if (c>0) summ++;
         return summ;
     }
 
---
+++ src/level4/lesson7/task004/task004.java
@@ -0,0 +1,60 @@
+package level4.lesson7.task004;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class task004 {
+    public static void main (String[] args) throws Exception {
+
+        int a = read();
+        int b = read();
+        int c = read();
+
+
+        System.out.println(count(a,b,c));
+    }
+
+    public static int count (int a, int b, int c) {
+
+        countNumber number = new countNumber();
+
+
+
+        if (a>0) {
+            countNumber.pos++;
+        }
+        else if (a<0) {
+            countNumber.neg++;
+        }
+
+        if (b>0) {
+            countNumber.pos++;
+        }
+        else if (b<0) {
+            countNumber.neg++;
+        }
+        if (c>0) countNumber.pos++;
+        else if (c<0) {
+            countNumber.neg++;
+        }
+        return number;
+    }
+
+
+    public static int read () throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String c = reader.readLine();
+        int nc = Integer.parseInt(c);
+        return nc;
+
+    }
+
+    public static class countNumber {
+
+        public static int pos;
+        public static int neg;
+
+    }
+}
+
