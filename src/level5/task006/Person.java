5f0467ecc96f297e2d2c22eb4b46e57443861464
denis <deoniska@me.com>
2018-12-15 11:43:44 +0300

    monday

---
+++ src/level5/task006/Person.java
@@ -0,0 +1,14 @@
+package level5.task006;
+/*
+Человечки
+*/
+public class Person {
+    private String name;
+    private int age;
+    private String address;
+    private char sex;
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/level5/task012/Circle.java
@@ -0,0 +1,14 @@
+package level5.task012;
+
+public class Circle {
+
+    private int centerX;
+    private int centerY;
+    private int radius;
+    private int width;
+    private String color;
+
+    public void initialize (int centerX, int centerY, int radius) {
+
+    }
+}
